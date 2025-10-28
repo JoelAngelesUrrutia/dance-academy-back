364a47198fd713a141a550982e028e9489fd9885
Joel Angeles <jangeles@medicavial.com.mx>
2025-10-28 15:48:17 -0600

    Commit inicial: PostgreSQL y Spring Boot funcionando en develop

---
+++ app/src/main/java/com/jangeles/dance_academy_api/DanceAcademyApiApplication.java
@@ -0,0 +1,13 @@
+package com.jangeles.dance_academy_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanceAcademyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanceAcademyApiApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/jangeles/dance_academy_api/DanceAcademyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jangeles.dance_academy_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanceAcademyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
